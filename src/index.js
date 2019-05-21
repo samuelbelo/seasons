0297daabba9f6ac708ebbd087f54ad1a71065430
Samuel <sam-uka-54@hotmail.com>
2019-05-21 13:52:42 -0300

    Assign a state object on a function from getPosition()

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
+//import SeasonDisplay from './SeasonDisplay'
 
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
+        this.state = { lat: null }
 
-    );
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
 
-    return <div>Olá mundo!</div>
+                //everytime I want to update a state, I need to call setState!!!!!!
+                this.setState({ lat: position.coords.latitude})
+                //remember this!!!! setState!!!!!
+
+                // I will never make anything like the follow:
+                //>>>>>>>    this.state.lat = position.coords.latitude   <<<<<<<
+                //this is kinda AWFUL, TERRIBLE. Don't ever assign a state directly on the state.
+                
+            },
+            (error) => console.log(error)
+    
+        );  
+    }
+    
+    
+    render() {
+
+        return <div>Latitude: {this.state.lat}</div>
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root')); 
